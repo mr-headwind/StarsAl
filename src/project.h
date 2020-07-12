0a86a216a761c4d1d6cbd9e7b21903e0198dbba5
Anthony Buckley <tony.buckley000@gmail.com>
2020-07-12 18:43:24 +1000

    work in progress

---
+++ src/project.h
@@ -0,0 +1,83 @@
+/*
+**  Copyright (C) 2021 Anthony Buckley
+** 
+**  This file is part of StarsAl.
+** 
+**  StarsAl is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**  
+**  StarsAl is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**  
+**  You should have received a copy of the GNU General Public License
+**  along with StarsAl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+/*
+** Description:	Project details
+**
+** Author:	Anthony Buckley
+**
+** History
+**	12-Jun-2021	Initial
+**
+*/
+
+
+/* Includes */
+
+#include <gtk/gtk.h>
+
+
+// Structure(s) to contain all project details.
+
+#ifndef PROJECT_H
+#define PROJECT_H
+
+typedef struct _ProjectData
+{
+    /* Ui */
+    GtkWidget *window;
+    GtkWidget *nm_box, *img_grid, *dark_grid, *btn_box, *cntl_box;
+    GtkWidget *proj_nm_lbl, *proj_nm;
+    GtkWidget *img_lbl, *img_list, *img_sel_btn;
+    GtkWidget *darks_lbl, *darks_list, *darks_sel_btn;
+    GtkWidget *save_btn, *cancel_btn;
+
+    /* Data */
+    char *project_name;
+    char *proj_path;
+    int status;
+    GList *images_gl;
+    GList *darks_gl;
+} ProjectData;
+
+
+typedef struct _ImgMeta
+{
+    char *camera;
+    char *img_type;
+    char *img_date;
+    int img_width;
+    int img_height;
+    char *wh_unit;
+    int iso;
+    int exposure;
+    char *exp_unit;
+} ImgMeta;
+
+
+typedef struct _Image
+{
+    char *img_nm;
+    char *img_path;
+    ImgMeta img_meta;
+} Image;
+
+#endif
